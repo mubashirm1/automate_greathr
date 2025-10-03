b39150e2c05d545dfe74d9f9491a44bd3ee24db2
mubashirm1 <mubashirmathoriyel@gmail.com>
2025-10-03 08:01:10 +0530

    updated code to fix bug and added wait till load feature

--- module/greathr_logger.py
+++ module/greathr_logger.py
@@ -1,5 +1,22 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import time
+
+def scroll_until_found(driver, max_scrolls=5):
+    for i in range(max_scrolls):
+        try:
+            shadow_host = WebDriverWait(driver,4).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "gt-button")))
+            shadow_root = driver.execute_script("return arguments[0].shadowRoot", shadow_host)
+            sign_button = WebDriverWait(shadow_root,4).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'button[name="primary"]')))
+            return sign_button
+        except Exception as e:
+            print(e)
+            time.sleep(2)
+            driver.execute_script("window.scrollBy(0, 100);")  # scroll down a bit
+    raise TimeoutException(f"Element not found after {max_scrolls} scrolls")
+
 
 def greathr_logger(driver,user_name:str, password:str) -> None:
     """
@@ -13,25 +30,23 @@ def greathr_logger(driver,user_name:str, password:str) -> None:
     #go to the site
     driver.get("https://vcreatek.greythr.com/uas/portal/auth")
     time.sleep(5)
-    #enter username
     driver.set_window_size(3840, 2160)  # set window size
-    time.sleep(2)
-    username_input = driver.find_element(By.ID,"username")
+
+    #enter username
+    username_input = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "username")))
     username_input.send_keys(user_name)
-    time.sleep(1)
     #enter password
-    password_input = driver.find_element(By.ID,"password")
+    password_input = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID,"password")))
     password_input.send_keys(password)
-    time.sleep(1)
     #click login button
-    login_button = driver.find_element(By.XPATH, '//button[text()=" Login "]')
+    login_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//button[text()=" Login "]')))
     login_button.click()
     time.sleep(5)
+    driver.set_window_size(1920, 1080)          # set window size 
+
     #click signIn/signOut button
-    driver.set_window_size(1920, 1080)          # set window size
-    driver.execute_script("window.scrollBy(0, 200);") # scroll a little down
+    sign_button = scroll_until_found(driver)
+    sign_button.click()
     time.sleep(2)
-    shadow_host = driver.find_element(By.CSS_SELECTOR, "gt-button")
-    shadow_root = driver.execute_script("return arguments[0].shadowRoot", shadow_host)
-    sign_in_button = shadow_root.find_element(By.CSS_SELECTOR, 'button[name="primary"]')
-    sign_in_button.click()
+    current_button = scroll_until_found(driver)
+    return current_button.text
--- run_main.py
+++ run_main.py
@@ -1,5 +1,6 @@
 #if you are using task schedular run this py file
 import import_ipynb
 from main import main
-
+from warnings import filterwarnings
+filterwarnings('ignore')
 main()
